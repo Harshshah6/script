d86d275f25f21d453334c876757bd8a1b800b72e
LEGENDARY STREAMER <69447184+Harshshah6@users.noreply.github.com>
2024-07-30 19:42:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests
 git clone https://github.com/Harshshah6/local_manifests --depth 1 -b main .repo/local_manifests
 
 # rm -rf prebuilts/clang/host/linux-x86/
-rm -rf out/
+# rm -rf out/
 
 /opt/crave/resync.sh
 
